30cd3754537daff5779f3cdf5c1412bbe3ce05ed
booiljung <booiljung@me.com>
2019-07-16 13:30:27 +0900

    initial commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:dart_reflection_examples/dart_reflection_examples.dart' as dart_reflection_examples;
+
+main(List<String> arguments) {
+  print('Hello world: ${dart_reflection_examples.calculate()}!');
+}
---
+++ test/dart_reflection_examples_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_reflection_examples/dart_reflection_examples.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
